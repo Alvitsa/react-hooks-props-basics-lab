43398a36f0a586efc4a02bc04e5d34f16531aa69
+ <+>
2024-11-18 11:42:20 +0300

    create files

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({ bio, github, linkedin }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* Conditionally render the <p> tag if bio exists and is non-empty */}
+      {bio && <p>{bio}</p>}
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+
+      {/* Add the <Links /> component and pass github and linkedin as props */}
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
